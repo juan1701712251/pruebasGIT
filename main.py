e94d1c2323c63931806035fd98510fc9bb164c8f
juan1701712251 <juan.1701712251@ucaldas.edu.co>
2020-04-17 16:02:11 -0500

    Commit trabajando paralelamente con cabecera

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print('Hello GIT')
 print('Algo importante')
+
+print('Estoy haciendo cambios desde el master, probando el merge')
 print('Estoy con un bug pero he hecho cosas importantes en este momento')
 print('Trabajando en otra rama')

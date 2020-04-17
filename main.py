2a3c717ff9f06a2b4f4a22c2d2cc612ebe5b7ec0
juan1701712251 <juan.1701712251@ucaldas.edu.co>
2020-04-17 15:59:28 -0500

    Finalizada la cabecera  con diseño azul

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print('Hello GIT')
 print('Algo importante')
 
 print('Esta rama se está encargando de la cabecera de algo')
+print('Algo mas que hizo la rama cabecera')
 
 print('Estoy con un bug pero he hecho cosas importantes en este momento')
 print('Trabajando en otra rama')

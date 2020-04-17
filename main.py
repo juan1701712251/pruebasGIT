6e29e55c42621c040711a203a14638fc64ca79d4
juan1701712251 <juan.1701712251@ucaldas.edu.co>
2020-04-17 15:41:38 -0500

    Estructura iniciak de la caecera

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 print('Hello GIT')
 print('Algo importante')
+
+print('Esta rama se está encargando de la cabecera de algo')
+
 print('Estoy con un bug pero he hecho cosas importantes en este momento')
 print('Trabajando en otra rama')

8b957d790c94378cbac7490b39e6f378ab1f0f38
juan1701712251 <juan.1701712251@ucaldas.edu.co>
2020-04-17 16:31:33 -0500

    Solucione conflicto de las ramas

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 print('Hello GIT')
 print('Algo importante')
 
-<<<<<<< HEAD
 print('Estoy haciendo cambios desde el master, probando el merge')
-=======
 print('Esta rama se está encargando de la cabecera de algo')
 print('Algo mas que hizo la rama cabecera')
 
->>>>>>> cabecera
 print('Estoy con un bug pero he hecho cosas importantes en este momento')
 print('Trabajando en otra rama')
